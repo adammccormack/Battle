a6c3b871c418869c8b129ca52b489c826e31ecd6
Adam <adam.j.mccor@gmail.com>
2021-04-15 17:12:49 +0100

    'add implementing hit points'

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,16 @@
+feature 'Attacking' do
+  scenario 'attack Player 2' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content 'Charlotte attacked Mittens'
+  end
+
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Mittens: 100HP'
+    expect(page).to have_content 'Mittens: 90HP'
+  end
+end
